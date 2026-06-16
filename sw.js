ec8a3e4653eef276a9ac270832e13ff261b5b720
fispas <36760479+fispas@users.noreply.github.com>
2026-06-16 15:49:09 +0200

    first

---
+++ sw.js
@@ -0,0 +1,16 @@
+{
+  "name": "Canzoniere",
+  "short_name": "Canzoniere",
+  "description": "Il tuo libro accordi personale",
+  "start_url": "./",
+  "display": "standalone",
+  "background_color": "#161310",
+  "theme_color": "#161310",
+  "icons": [
+    {
+      "src": "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🎸</text></svg>",
+      "sizes": "any",
+      "type": "image/svg+xml"
+    }
+  ]
+}
