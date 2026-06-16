1a17d9983d315a2f030f4e304fd157e9e672d387
fispas <36760479+fispas@users.noreply.github.com>
2026-06-16 17:20:57 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,5 @@
-const CACHE_NAME = 'canzoniere-v1';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json'
-];
+const CACHE_NAME = 'canzoniere-v2';
+const ASSETS = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
